09ffe37905f60be077bb6731e093f17c11a2bdcf
mallikarjun <mallijangam@gmail.com>
2024-08-24 11:11:42 +0530

    Added mongo Db connenction

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const sample = 'Hello World';
+const express = require("express");
+const dotEnv = require("dotenv");
+const mongoose = require('mongoose');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const PORT = 5001;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+dotEnv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Mongo DB Connected Successfully'))
+    .catch(error => console.log(error));
+
+app.listen(PORT, () => {
+    console.log(`Server Running at ${PORT}`)
+});
+
